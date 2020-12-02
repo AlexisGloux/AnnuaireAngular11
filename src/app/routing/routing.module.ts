4e9fb065c11efee4bf99f99b86ba1c220cf52d92
Gloux Alexis <gloux10@gmail.com>
2020-12-02 14:39:34 +0100

    ajout de l'édition d'un contact

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {ContactNavComponent} from '../contact-nav/contact-nav.component';
-import {ContactCreateReactiveComponent} from '../contact-create-reactive/contact-create-reactive.component';
-import {ContactDetailComponent} from '../contact-detail/contact-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactNavComponent } from '../contact-nav/contact-nav.component';
+import { ContactCreateReactiveComponent } from '../contact-create-reactive/contact-create-reactive.component';
+import { ContactDetailComponent } from '../contact-detail/contact-detail.component';
+import { ContactEditComponent } from '../contact-edit/contact-edit.component';
 
 const routes: Routes = [
   { path: '', component: ContactNavComponent },
   { path: 'new', component: ContactCreateReactiveComponent },
-  { path: 'show/:id', component: ContactDetailComponent }
+  { path: 'show/:id', component: ContactDetailComponent },
+  { path: 'edit/:id', component: ContactEditComponent }
 ];
 
 @NgModule({
