ca764c6e01347329be92a692ea0a15e4759ef7b6
Gloux Alexis <gloux10@gmail.com>
2020-12-03 12:03:38 +0100

    récupération des données via http

--- src/app/contact-list.service.ts
+++ src/app/contact-list.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Contact, CONTACTS, NEXT_ID } from './fixtures/contacts';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactListService {
-  contacts = CONTACTS;
+  contacts = [];
   nextId = NEXT_ID;
+
+  constructor(private http: HttpClient) {
+    // setTimeout(fetch, 500);
+    this.fetch();
+  }
+
+  fetch(): void {
+    this.http.get('assets/contacts.json').subscribe(
+      (contacts: Contact[]) => {
+        this.contacts = contacts;
+        // this.contacts.push(...contacts);
+        // console.log(contacts);
+      });
+  }
 
   findAll(): Array<Contact> {
     return this.contacts;
